6d3f9b680c4bd47da6c444bce2380c6cc0888eed
Jeet Barot <jeetbarot1998@gmail.com>
2024-02-26 16:12:32 +0530

    Update Stock_sell_buy.py

--- Stock_sell_buy.py
+++ Stock_sell_buy.py
@@ -1,13 +1,20 @@
-def buy_and_sell(inp):
-    sell = max(inp)
-    buy = min(inp)
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        current_max_val = 0
+        potential_profit = 0
+        max_profit_val = 0
 
-    if inp.index(buy) < inp.index(sell):
-        profit = sell - buy
-    else:
-        profit = 0
-    return profit
-
-print(buy_and_sell([8, 10, 7, 5, 7, 15]))  # Maximum Profit: 10
-print(buy_and_sell([1, 2, 8, 1]))           # Maximum Profit: 7
-print(buy_and_sell([9,8,7,6,5,4,3,2,1]))
+        for price in reversed(prices):
+            # Update the current maximum value with the maximum of the current value and the previous maximum
+            current_max_val = max(current_max_val, price)
+            
+            # Calculate the potential profit by subtracting the current price from the current maximum value
+            potential_profit = current_max_val - price
+            
+            # Update the maximum profit with the maximum of the potential profit and the previous maximum profit
+            max_profit_val = max(potential_profit, max_profit_val)
+        return max_profit_val
