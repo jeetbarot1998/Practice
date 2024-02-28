0283460c814a723c3afe790aa3742a73fc751c20
Jeet Barot <jeetbarot1998@gmail.com>
2024-02-28 15:40:30 +0530

    Update linked_list.py

--- linked_list.py
+++ linked_list.py
@@ -1,14 +1,12 @@
 class _Node:
-    __slot__ = ['_element', '_next']
+    __slots__ = '_element', '_next'
 
-    def __init__(self, element, nextelem):
+    def __init__(self, element, next):
         self._element = element
-        self._next = nextelem
+        self._next = next
 
-
-class linkedList:
+class LinkedList:
     def __init__(self):
-        # Initially the linked list will be of len 0, has no head and tail
         self._head = None
         self._tail = None
         self._size = 0
@@ -19,16 +17,6 @@ class linkedList:
     def isempty(self):
         return self._size == 0
 
-    def addfirst(self, e):
-        if self.isempty():
-            newest = _Node(e, None)
-            self._head = newest
-            self._tail = newest
-        else:
-            newest = _Node(e, self._head)
-            self._head = newest
-        self._size += 1
-
     def addlast(self, e):
         newest = _Node(e, None)
         if self.isempty():
@@ -38,155 +26,91 @@ class linkedList:
         self._tail = newest
         self._size += 1
 
-    def addany(self, index, e):
-        p = self._head
-        i = 0
-        if index > self._size:
-            return self.addlast(e)
-
-        elif self.isempty() or index == 0:
-            self.addfirst(e)
-
+    def addfirst(self, e):
+        newest = _Node(e, None)
+        if self.isempty():
+            self._head = newest
+            self._tail = newest
         else:
-            while p:
-                if index == i:
-                    newest = _Node(e, p._next)
-                    p._next = newest
-                    self._size += 1
-                    break
-                i += 1
+            newest._next = self._head
+            self._head = newest
+        self._size += 1
+
+    def addany(self, e, position):
+        newest = _Node(e, None)
+        p = self._head
+        i = 1
+        while i < position-1:
+            p = p._next
+            i = i + 1
+        newest._next = p._next
+        p._next = newest
+        self._size += 1
 
     def removefirst(self):
-        # only delete if the length is not 0
         if self.isempty():
             print('List is empty')
             return
         e = self._head._element
         self._head = self._head._next
         self._size -= 1
-        # If the length is 0 now, then set the tail as None
-        if self.isempty() or self._size == 1:
+        if self.isempty():
             self._tail = None
         return e
 
     def removelast(self):
-        # only delete if the length is not 0
         if self.isempty():
             print('List is empty')
             return
-        index = 1
-        # If the length is 1, then set the head,tail as None and size as 0
-        if self._size == 1:
-            element_deleted = self._head._element
-            self._tail = None
-            self._head = None
-            self._size = 0
-            return element_deleted
         p = self._head
-        # traverse to the 2nd last element of the linked list
-        while index < self._size - 1:
+        i = 1
+        while i < len(self) - 1:
             p = p._next
-            index += 1
-        # Now we are the 2nd last node of the linked list
-        element_deleted = p._next._element
+            i = i + 1
         self._tail = p
+        p = p._next
+        e = p._element
         self._tail._next = None
         self._size -= 1
-        return element_deleted
+        return e
 
-    def removeany(self,index):
-        if self.isempty():
-            print('List is empty')
-            return
-        i = 1
-        if index == 1:
-            return  self.removefirst()
-        elif index == self._size:
-            return self.removelast()  
-        if index not in range(1,self._size):
-            return 'list index out of range'
-        
+    def removeany(self, position):
         p = self._head
-        while i < index - 1:
+        i = 1
+        while i < position - 1:
             p = p._next
-            i += 1
-        # Now we are the previous node of the element to deleted
-        element_deleted = p._next._element
+            i = i + 1
+        e = p._next._element
         p._next = p._next._next
-        return element_deleted
+        self._size -= 1
+        return e
 
     def display(self):
         p = self._head
         while p:
-            print(p._element, end='-->')
+            print(p._element,end='-->')
             p = p._next
         print()
 
-    def search(self, key):
+    def search(self,key):
         p = self._head
         index = 0
         while p:
             if p._element == key:
                 return index
             p = p._next
-            index += 1
+            index = index + 1
         return -1
 
-    def insert_sorted(self, key):
-        newest = _Node(e,None)
-        if self.isempty():
-            self._head = newest
-        else:
-            p = self._head
-            q = self._head
-            # traverse to where the node is to be inserted
-            while p and p._element < key:
-                q = p
-                p = p._next
-            if p == self._head:
-                newest._next = self._head
-                self._head = newest
-            else:
-                newest._next = q._next
-                q._next = newest
-        self._size += 1
-        
-
-
-
-L = linkedList()
-L.addlast(7)
-L.display()
-print('length is', len(L))
-L.addlast(18)
-L.addlast(3)
-L.display()
-print('length is', len(L))
-value_to_search = 12
-print(f'searched index of {value_to_search} is:', L.search(value_to_search))
-# L.addfirst(1)
-# L.display()
+L = LinkedList()
 L.addlast(7)
 L.addlast(4)
 L.addlast(12)
-L.addfirst(9)
-# L.display()
-L.addany(0, 99)
+L.addlast(8)
+L.addlast(3)
 L.display()
-print('length is', len(L))
-print('Element removefirst is', L.removefirst())
+print('Size:',len(L))
+ele = L.removeany(3)
 L.display()
-print('line 154: length is', len(L))
-print('line 155: Element removefirst is', L.removefirst())
-L.display()
-print('length is', len(L))
-print('Element removelast is', L.removelast())
-L.display()
-print('line 160: length is', len(L))
-print('line 161: Element removelast is', L.removelast())
-L.display()
-print('line 163: length is', len(L))
-print('element removed using removeany is:', L.removeany(1))
-L.display()
-print('line 166: length is', len(L))
-
+print('Size:',len(L))
+print('Removed Element:',ele)
